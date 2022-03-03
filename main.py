e2eb4ef4f7f8eaaadfa28f9fac8cee12dc59cc0a
Stanislav Kholmanskikh <stanislav.kholmanskikh@bell-sw.com>
2022-03-03 17:15:00 +0300

    Add primitive logging for development

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import logging
+
 from installer.application import Application
 from installer.palette import PALETTE
 
+logging.basicConfig(filename='installer.log', filemode='w')
 
 app = Application(header='Alpaca Linux Installation',
                   palette=PALETTE)
