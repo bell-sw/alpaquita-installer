8de1ba211c9af6426a8ed096360ad460be7861f0
Alexey Kodanev <aleksei.kodanev@bell-sw.com>
2022-03-25 11:08:39 +0300

    repos: allow to select libc type and fix repos urls

--- alpaca_installer/controllers/repo.py
+++ alpaca_installer/controllers/repo.py
@@ -1,13 +1,11 @@
 import urllib
 import yaml
 import logging
+import os
 from .controller import Controller
 from alpaca_installer.views.repo import RepoView
 
 log = logging.getLogger('controllers.repo')
-
-class RepoException(Exception):
-    pass
 
 class RepoController(Controller):
     def __init__(self, app):
@@ -15,7 +13,8 @@ class RepoController(Controller):
         self._repo_base_url = 'https://packages.bell-sw.com'
         self._repos = []
         self._release = self.get_os_release().get('VERSION_ID', '').split('.')[0]
-        self._release = 'v' + self._release if self._release else 'stream'
+        self._release = self._release if self._release else 'stream'
+        self._libc_type = 'musl' if os.path.exists('/lib/ld-musl-x86_64.so.1') else 'glibc'
 
     def get_os_release(self):
         res = {}
@@ -26,19 +25,22 @@ class RepoController(Controller):
         return res
 
     def make_ui(self):
-        return RepoView(self, self._repo_base_url)
+        return RepoView(self, self._repo_base_url, self._libc_type)
 
-    def done(self, repo_base_url: str):
+    def done(self, repo_base_url: str, libc_type: str):
         self._repo_base_url = repo_base_url
+        self._libc_type = libc_type
         self._app.next_screen()
 
     def cancel(self):
         self._app.prev_screen()
 
-    def get_repos(self, url):
+    def get_repos(self, url, libc):
         self._repos = []
         for name in [ 'core', 'universe' ]:
-            self._repos.append(urllib.parse.quote(f'{url}/alpaca/{self._release}/{name}', safe='/:'))
+            self._repos.append(urllib.parse.quote(
+                               f'{url}/alpaca/{libc}/{self._release}/{name}',
+                               safe='/:'))
         return self._repos
 
     def to_yaml(self):
--- alpaca_installer/views/repo.py
+++ alpaca_installer/views/repo.py
@@ -3,48 +3,68 @@ import urwid
 from subiquitycore.view import BaseView
 from subiquitycore.ui.form import (
     Form,
+    SubForm,
     URLField,
     ReadOnlyField,
+    BooleanField,
+    ChoiceField,
+    SubFormField,
     NO_HELP
 )
 
+class UrlsForm(SubForm):
+    field0 = ReadOnlyField('', help=NO_HELP)
+    field1 = ReadOnlyField('')
+
 class RepoForm(Form):
     cancel_label = 'Back'
-    repo_base_url = URLField('Base URL:')
-    repo_field0 = ReadOnlyField('Repositories:', help=NO_HELP)
-    repo_field1 = ReadOnlyField('', help=NO_HELP)
 
-    def __init__(self):
-        super().__init__()
-        self.repo_fields = [self.repo_field0, self.repo_field1]
+    libc_type = ChoiceField('libc type:', choices=['musl', 'glibc'])
+    repo_base_url = URLField('Base URL:')
+    repo_fields = SubFormField(UrlsForm, 'Repositories:')
 
 class RepoView(BaseView):
     title = 'Installation Source'
-    excerpt = ('Specify the URL of the repository server that will be used to install the packages')
+    excerpt = ('Specify the URL of the repository server that '
+               'will be used to install the packages')
 
-    def __init__(self, controller, repo_base_url: str):
+    def __init__(self, controller, repo_base_url: str, libc_type: str):
         self._controller = controller
 
         self._form = RepoForm()
         self._form.repo_base_url.widget.value = repo_base_url
-        self.set_repos_text(repo_base_url)
+        self._form.libc_type.widget.value = libc_type
+        self._libc = libc_type
+        self._url = repo_base_url
+        self._set_repos_text()
 
         urwid.connect_signal(self._form, 'submit', self.done)
         urwid.connect_signal(self._form, 'cancel', self.cancel)
-        urwid.connect_signal(self._form.repo_base_url.widget, 'change', self.update_repos_text)
+        urwid.connect_signal(self._form.repo_base_url.widget, 'change',
+                             self._update_repos_text_url)
+        urwid.connect_signal(self._form.libc_type.widget, 'select',
+                             self._update_repos_text_libc)
 
         super().__init__(self._form.as_screen(excerpt=self.excerpt,
                                               focus_buttons=True))
 
     def done(self, sender):
-        self._controller.done(self._form.repo_base_url.widget.value)
+        self._controller.done(self._form.repo_base_url.widget.value,
+                              self._form.libc_type.widget.value)
 
     def cancel(self, sender=None):
         self._controller.cancel()
 
-    def set_repos_text(self, url):
-        for r, w in zip(self._controller.get_repos(url), self._form.repo_fields):
-            w.widget.value = r
+    def _set_repos_text(self):
+        d = {}
+        for r, k in zip(self._controller.get_repos(self._url, self._libc), ['field0', 'field1']):
+            d[k] = r
+        self._form.repo_fields.widget.value = d
 
-    def update_repos_text(self, sender, url):
-        self.set_repos_text(url)
+    def _update_repos_text_url(self, sender, url):
+        self._url = url
+        self._set_repos_text()
+
+    def _update_repos_text_libc(self, sender, libc):
+        self._libc = libc
+        self._set_repos_text()
