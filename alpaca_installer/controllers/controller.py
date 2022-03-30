f43ec42809cfe36a5d289242546093414f2ecdb9
Stanislav Kholmanskikh <stanislav.kholmanskikh@bell-sw.com>
2022-03-30 16:13:02 +0300

    controllers/controller: add more typing

--- alpaca_installer/controllers/controller.py
+++ alpaca_installer/controllers/controller.py
@@ -1,6 +1,12 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from alpaca_installer.app.application import Application
+
 
 class Controller:
-    def __init__(self, app):
+    def __init__(self, app: Application):
         self._app = app
 
     def to_yaml(self) -> str:
