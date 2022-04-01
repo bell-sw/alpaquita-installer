ab3111897c89c9640c6ef42a97553d3130549bac
Stanislav Kholmanskikh <stanislav.kholmanskikh@bell-sw.com>
2022-04-01 14:08:00 +0300

    Add ServicesInstaller

--- alpaca_installer/controllers/installer.py
+++ alpaca_installer/controllers/installer.py
@@ -10,6 +10,7 @@ from alpaca_installer.views.installer import InstallerView
 from alpaca_installer.installers.storage import StorageInstaller
 from alpaca_installer.installers.repo import RepoInstaller
 from alpaca_installer.installers.packages import PackagesInstaller
+from alpaca_installer.installers.services import ServicesInstaller
 from alpaca_installer.installers.swapfile import SwapfileInstaller
 from alpaca_installer.installers.timezone import TimezoneInstaller
 from alpaca_installer.installers.users import UsersInstaller
@@ -79,6 +80,7 @@ class BaseInstallerController(Controller, EventReceiver):
             storage_installer,
             RepoInstaller(target_root=target_root, config=config, event_receiver=self),
             pkgs_installer,
+            ServicesInstaller(target_root=target_root, config=config, event_receiver=self),
             SwapfileInstaller(target_root=target_root, config=config, event_receiver=self),
             TimezoneInstaller(target_root=target_root, config=config, event_receiver=self),
             UsersInstaller(target_root=target_root, config=config, event_receiver=self),
---
+++ alpaca_installer/installers/services.py
@@ -0,0 +1,74 @@
+from .installer import Installer
+from .utils import read_key_or_fail
+
+
+# Optional
+#
+# At first the 'disabled' list is processed, then the 'enabled' one
+#
+# services:
+#   disabled: ['svc1', 'svc2']
+#   enabled: ['svc3']
+#
+
+
+class ServicesInstaller(Installer):
+    def __init__(self, target_root: str, config: dict, event_receiver):
+        yaml_tag = 'services'
+        super().__init__(name=yaml_tag, config=config,
+                         event_receiver=event_receiver,
+                         data_type=dict, data_is_optional=True,
+                         target_root=target_root)
+
+        lists = {'disabled': [], 'enabled': []}
+
+        if self._data is not None:
+            for name in lists:
+                if name not in self._data:
+                    continue
+
+                lst = read_key_or_fail(self._data, name, list)
+                if not all(isinstance(x, str) for x in lst):
+                    raise ValueError("All '{}/{}' elements must be strings".format(yaml_tag, name))
+                lists[name] = lst
+
+        self._disabled = lists['disabled']
+        self._enabled = lists['enabled']
+
+    def apply(self):
+        pass
+
+    def post_apply(self):
+        self._event_receiver.start_event('Enabling base services')
+        for svc, runlevel in [('agetty.tty1', 'boot'),
+                              ('acpid', 'default'),
+                              ('bootmisc', 'boot'),
+                              ('crond', 'default'),
+                              ('dmesg', 'sysinit'),
+                              ('hostname', 'boot'),
+                              ('hwclock', 'boot'),
+                              ('killprocs', 'shutdown'),
+                              ('modules', 'boot'),
+                              ('mount-ro', 'shutdown'),
+                              ('networking', 'boot'),
+                              ('savecache', 'shutdown'),
+                              ('swap', 'boot'),
+                              ('sysctl', 'boot'),
+                              ('syslog', 'boot'),
+                              ('udev', 'sysinit'),
+                              ('udev-settle', 'sysinit'),
+                              ('udev-trigger', 'sysinit'),
+                              ('urandom', 'boot')]:
+            self.enable_service(service=svc, runlevel=runlevel)
+
+        if self._disabled:
+            self._event_receiver.start_event('Disabling services: {}'.format(sorted(self._disabled)))
+
+            for svc in self._disabled:
+                self.disable_service(service=svc, runlevel='default')
+
+        if self._enabled:
+            self._event_receiver.start_event('Enabling services: {}'.format(sorted(self._enabled)))
+
+            for svc in self._enabled:
+                self.enable_service(service=svc, runlevel='default')
