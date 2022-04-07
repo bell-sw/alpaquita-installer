2010e0fe674a70c9aa9ff6119bac0fd4d34dc046
Alexey Kodanev <aleksei.kodanev@bell-sw.com>
2022-04-07 16:59:00 +0300

    secureboot: move info text to excerpt and fix style

--- alpaca_installer/views/secureboot.py
+++ alpaca_installer/views/secureboot.py
@@ -9,25 +9,22 @@ from subiquitycore.ui.form import (
     ReadOnlyField,
 )
 
-
 class SecureBootForm(Form):
     cancel_label = 'Back'
-    info_text = ReadOnlyField('', (
-        'Alpaca Linux provides pre-bootloader "shim" with a built-in certificate, '
+    install_shim = BooleanField('Install shim and signed grub bootloaders for SB')
+
+class SecureBootView(BaseView):
+    title = 'Secure Boot'
+    excerpt = ('info_minor', (
+        'Alpaca Linux provides pre-bootloader shim with a built-in certificate, '
         'signed MOKManager utility, grub and kernel (automatically locked down if '
         'Secure Boot (SB) is enabled).\n\n'
-        'The currently provided shim efi image is not yet signed by Microsoft, so '
+        'The currently provided shim EFI image is not yet signed by Microsoft, so '
         'it cannot be loaded with SB enabled using Microsoft certificates. For it '
         'to work, the shim must be signed with your own trusted SB key from UEFI '
         'Signature Database (db) or the shim hash added there.\n\n'
         'If the option is selected, sbsigntool, efitools and mokutil packages will '
-        'also be installed.'
-        ))
-    install_shim = BooleanField('Install "shim" and signed "grub" bootloaders for SB')
-
-
-class SecureBootView(BaseView):
-    title = 'Secure Boot'
+        'also be installed.'))
 
     def __init__(self, controller, install_shim):
         self._controller = controller
@@ -36,7 +33,8 @@ class SecureBootView(BaseView):
 
         urwid.connect_signal(self._form, 'submit', self.done)
         urwid.connect_signal(self._form, 'cancel', self.cancel)
-        super().__init__(self._form.as_screen(focus_buttons=True))
+        super().__init__(self._form.as_screen(excerpt=self.excerpt,
+                                              focus_buttons=True))
 
     def done(self, sender):
         self._controller.done(self._form.install_shim.widget.value)
