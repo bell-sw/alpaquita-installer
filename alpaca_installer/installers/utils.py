94405a53a0e4063e672183401c3cdcb77a4c1825
Stanislav Kholmanskikh <stanislav.kholmanskikh@bell-sw.com>
2022-04-22 14:18:48 +0300

    installers/utils: add a couple of validation helpers

--- alpaca_installer/installers/utils.py
+++ alpaca_installer/installers/utils.py
@@ -2,7 +2,7 @@
 #  SPDX-License-Identifier:  AGPL-3.0-or-later
 
 import re
-from typing import Type, TypeVar, Optional
+from typing import Type, TypeVar, Iterable, Optional
 
 TV = TypeVar(name='TV')
 
@@ -16,6 +16,20 @@ def read_key_or_fail(data: dict, key: str, value_type: Type[TV],
         raise ValueError("'{}' must be of type '{}', but is '{}'".format(
             error_label, str(value_type), type(value)))
     return value
+
+
+def validate_types_of_elements(iterable: Iterable, item_type: Type[TV], error_label: str):
+    for x in iterable:
+        if not isinstance(x, item_type):
+            raise ValueError("All '{}' elements must be of type '{}', but '{}' is of type '{}'".format(
+                error_label, item_type, x, type(x)))
+
+
+def read_list(data: dict, key: str, item_type: Type[TV], error_label: str) -> list[TV]:
+    lst = read_key_or_fail(data, key, list, error_label=error_label)
+    if lst:
+        validate_types_of_elements(lst, item_type=item_type, error_label=error_label)
+    return lst
 
 
 def str_size_to_bytes(size: str) -> int:
