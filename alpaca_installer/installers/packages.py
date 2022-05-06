0c41dddfa9b230eb698fc1ce391b62e5e41a33a4
Stanislav Kholmanskikh <stanislav.kholmanskikh@bell-sw.com>
2022-05-06 10:42:29 +0300

    PackagesInstaller: add more yaml validation

--- alpaca_installer/installers/packages.py
+++ alpaca_installer/installers/packages.py
@@ -3,17 +3,21 @@
 
 from alpaca_installer.common.utils import run_cmd
 from .installer import Installer
+from .utils import read_list
 
 
 class PackagesInstaller(Installer):
     def __init__(self, target_root: str, config: dict, event_receiver):
-        super().__init__(name='extra_packages', config=config,
+        yaml_tag = 'extra_packages'
+        super().__init__(name=yaml_tag, config=config,
                          event_receiver=event_receiver,
                          target_root=target_root,
                          data_type=list,
                          data_is_optional=True)
         if self._data:
-            for pkg in self._data:
+            extra_pkgs = read_list(config, key=yaml_tag, item_type=str,
+                                   error_label=yaml_tag)
+            for pkg in extra_pkgs:
                 self.add_package(pkg)
 
         self.add_package('alpaca-base')
