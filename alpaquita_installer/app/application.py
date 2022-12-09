bell-sw/alpaquita-installer

commit 1f1286fb747e21fcf613b3bdb46a60eab44fd478

Display a more correct min disk size in HelpMsg
Calculation of the minimum size of a disk must take into account all partitions.

alpaquita_installer/app/application.py
@@ -48,8 +48,9 @@ MIN_SHOW_PROGRESS_TIME = 1.0
 class ApplicationUI(urwid.WidgetWrap):
     block_input = False
 
-    def __init__(self):
-        self._help_msg = HelpMsgStretchy(self)
+    def __init__(self, app: Application):
+        self._help_msg = HelpMsgStretchy(self,
+                                         min_disk_size=app.controller('StorageController').min_disk_size)
         self._shown_help_msg = None
 
         self._title = urwid.Text('Title', align='left')
@@ -170,7 +171,7 @@ class Application:
 
         self._ctrl_idx = 0
 
-        self.ui = self.make_ui()
+        self.ui = self.make_ui(self)
         self._palette = palette
 
         atexit.register(self.cleanup)

alpaquita_installer/controllers/storage.py
@@ -143,6 +143,15 @@ class StorageController(Controller):
                                crypto_passphrase=self._crypto_passphrase)
         return StorageView(self, self._available_disks[:], data)
 
+    @property
+    def min_disk_size(self) -> float:
+        size = self.ROOT_MIN_SIZE + self.BOOT_SIZE
+        if self._app.is_efi():
+            size += self.BOOT_EFI_SIZE
+        else:
+            size += self.BIOS_BOOT_SIZE
+        return size
+
     def done(self, data: StorageViewData):
         needs_reset = False
         if self._selected_disk != data.selected_disk:

alpaquita_installer/app/help.py
@@ -25,12 +25,12 @@ This program will guide you through all the steps to install Alpaquita Linux on 
 The procedure requires a disk minimum of {:.1f} GB size and an active network connection to the Internet or a private repository with APK packages.
 
 Additional shell sessions are available on other virtual terminals. You can switch to them using Ctrl+Alt+Fn or Alt+Left/Right key combinations.
-""".format(StorageController.ROOT_MIN_SIZE / StorageController.GB)
+"""
 
-    def __init__(self, app_ui: ApplicationUI):
+    def __init__(self, app_ui: ApplicationUI, min_disk_size: float):
         self._app_ui = app_ui
         _ok_btn = ok_btn('Close', on_press=self._close)
-        super().__init__('Help', [urwid.Text(self.MSG),
+        super().__init__('Help', [urwid.Text(self.MSG.format(min_disk_size / StorageController.GB)),
                                   urwid.Text(''),
                                   button_pile([_ok_btn])], 0, 2)
 

commit fcc4752b42fba9b5dd8b74bafe305698f331a225

Fix the display of min disk size in the config file mode
Commit 1f1286fb ("Display a more correct min disk size in HelpMsg")
introduced a bug. The problem was that when we ran the installer
with a config file, it tried to initialize the UI and failed, because
there was no instance of StorageController.

Let's fix that by moving the calculation of min_disk_size from
StorageController to Application.

alpaquita_installer/app/application.py
@@ -50,7 +50,7 @@ class ApplicationUI(urwid.WidgetWrap):
 
     def __init__(self, app: Application):
         self._help_msg = HelpMsgStretchy(self,
-                                         min_disk_size=app.controller('StorageController').min_disk_size)
+                                         min_disk_size=app.min_disk_size)
         self._shown_help_msg = None
 
         self._title = urwid.Text('Title', align='left')
@@ -205,6 +205,15 @@ class Application:
     @property
     def copy_config(self) -> bool:
         return self._copy_config
+
+    @property
+    def min_disk_size(self) -> float:
+        size = StorageController.ROOT_MIN_SIZE + StorageController.BOOT_SIZE
+        if self.is_efi():
+            size += StorageController.BOOT_EFI_SIZE
+        else:
+            size += StorageController.BIOS_BOOT_SIZE
+        return size
 
     def is_efi(self) -> bool:
         return os.path.exists('/sys/firmware/efi')

alpaquita_installer/controllers/storage.py
@@ -144,15 +144,6 @@ class StorageController(Controller):
                                crypto_passphrase=self._crypto_passphrase)
         return StorageView(self, self._available_disks[:], data)
 
-    @property
-    def min_disk_size(self) -> float:
-        size = self.ROOT_MIN_SIZE + self.BOOT_SIZE
-        if self._app.is_efi():
-            size += self.BOOT_EFI_SIZE
-        else:
-            size += self.BIOS_BOOT_SIZE
-        return size
-
     def done(self, data: StorageViewData):
         if self._selected_disk != data.selected_disk:
             self._selected_disk = data.selected_disk