cb6a9748275ef33089a93a34b2f74e5ee3989441
Stanislav Kholmanskikh <stanislav.kholmanskikh@bell-sw.com>
2023-11-15 10:57:08 +0300

    Update the version to 0.7.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import setuptools
 
 setuptools.setup(
     name='alpaquita_installer',
-    version="0.7.7",
+    version="0.7.8",
     description="Installer of Alpaquita-like Linux distributions",
     long_description="",
     author='BellSoft',
